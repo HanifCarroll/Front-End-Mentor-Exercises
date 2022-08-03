fc21844a801ab45b838d540acacb73645beeb633
hcarroll <hcarroll@leagueapps.com>
2022-08-03 10:03:40 -0500

    Product Preview Card - Finish mobile view

---
+++ product-preview-card-component/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'cream': 'hsl(30, 38%, 92%)',
+        'light-green-1': '#3f866e',
+        'light-green-2': '#3c8169',
+      },
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat'],
+      'fraunces': ['Fraunces'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
